35c95e59c780ba61a5cdcd7d291065d0a810aa7d
Sironysos <les.reseaux.d.alice@gmail.com>
2024-11-19 12:05:36 +0100

    Exo 13 nouveau TD

--- src/main/java/exercises/exercise13/VisitorPostFix.java
+++ src/main/java/exercises/exercise13/VisitorPostFix.java
@@ -1,23 +1,28 @@
 package exercises.exercise13;
-
+//Implémentez en Java un visiteur permettant d’afficher dans la console et en notation post-fixée la formule arithmétique que représente l’arbre
 public class VisitorPostFix implements VisitorTree {
 	@Override
 	public void visitTree(final Tree tree) {
-
+		System.out.println("Visit arbre " + tree.name);
+		tree.root.accept(this);
 	}
 
 	@Override
 	public void visitPlusNode(final PlusNode n) {
-
+		n.leftNode.accept(this);
+		n.rightNode.accept(this);
+		System.out.print(" +");
 	}
 
 	@Override
 	public void visitSubNode(final SubNode n) {
-
+		n.leftNode.accept(this);
+		n.rightNode.accept(this);
+		System.out.print(" -");
 	}
 
 	@Override
 	public void visitValueNode(final ValueNode n) {
-
+		System.out.print(" "+n.val);
 	}
 }
